70b59734a55baf2790ed7019895dcdbcb7f05bc8
Alizeyukoo <1127278741@qq.com>
2022-02-07 18:08:23 +0800

    idea first commit

---
+++ idea-git/learn-git/src/main/java/domain/GitTest.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello github!");
+        System.out.println("hello gitee!");
+    }
+
+}
